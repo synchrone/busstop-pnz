064aa306005b485197dc593ed5f209eec96bad21
Alexander Bogdanov <syn@li.ru>
2013-02-02 14:53:01 +0400

    Fixing checker

--- application/classes/Task/Check.php
+++ application/classes/Task/Check.php
@@ -8,10 +8,11 @@ class Task_Check extends Minion_Task
 
     public function _execute(array $params){
         $stations = Cache::instance()->get('stations');
+        /** @var $station Model_Station */
         $station = current($stations);
 
         try{
-            Model_Remote::stations($station->id1);
+            Model_Remote::stations($station->id1,null);
         }catch (Exception $e){
             $msg = Kohana_Exception::text($e).PHP_EOL.$e->getTraceAsString();
             mail(self::HUMAN,'WhenBus forecast fails!',$msg);
