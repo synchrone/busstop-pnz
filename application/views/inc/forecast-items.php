9c1ec027b99a3f1cc2a6e3874cb76cb5ab1f4c25
trom58al <lt301@mail.ru>
2013-05-30 14:53:57 +0400

    final edit views route(82c)

--- application/views/inc/forecast-items.php
+++ application/views/inc/forecast-items.php
@@ -2,14 +2,9 @@
 
 foreach($items as $item)
 {
-    if(strlen($item->route_num)<=2){
-        $route_num=$item->route_num.'&nbsp;&nbsp;';
-    }else{
-        $route_num=$item->route_num;
-    }
-    printf("<li><img src='img/%s.png' alt='%s' class='ui-li-icon'>%s &rarr;%s
+    printf("<li><img src='img/%s.png' alt='%s' class='ui-li-icon'><span class='ui-li-route'>%s</span> &rarr;%s
             <span class='ui-li-count'>%s</span></li>",
-        $item->route_type, $item->route_num, $route_num, $item->where_go,
+        $item->route_type, $item->route_num, $item->route_num, $item->where_go,
         $item->arrive_time()
     );
 }
